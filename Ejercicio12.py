882342177b04bf6c35934a9482c65c2c12a68791
Yirgrin <ybermuddez@gmail.com>
2022-02-28 13:56:21 -0600

    Ejercicio 12. Python
    Asiganción #2
    Indicación: 12.	Introducir una cadena de caracteres e indicar si es un palíndromo. Una palabra palíndroma es aquella que se lee igual adelante que atrás.

---
+++ Ejercicio12.py
@@ -0,0 +1,8 @@
+palabra= input ("Ingrese una palabra ")
+
+inversa= palabra [:: -1]
+
+if inversa in palabra: 
+    print ("Es una palabra palíndromo")
+else: 
+    print ("No es una palabra palíndromo")
