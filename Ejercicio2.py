820b796b32be677fcd7b78306f7daa4f8851f76f
Yirgrin <ybermuddez@gmail.com>
2022-02-28 13:49:21 -0600

    Ejercicio 2. Python
    Asiganción #2
    Indicación: sacar el promedio de 3 notas ingresadas por el teclado, y debe de mostrar el promedio.

---
+++ Ejercicio2.py
@@ -0,0 +1,10 @@
+
+print ("Ingrese las notas")
+
+nota1 = int (input ())
+nota2 = int (input ())
+nota3 = int (input ())
+
+promedio = (nota1 + nota2 + nota3) / 3
+
+print ("El promedio de las notas es igual a" , promedio)
