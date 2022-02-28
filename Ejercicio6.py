65a858e15b8e12060806661d56e3558a368e0d99
Yirgrin <ybermuddez@gmail.com>
2022-02-28 13:52:23 -0600

    Ejercicio 6. Python
    Asiganción #2
    Indicación: determine si una cadena de texto introducida por el usuario tiene una logitud mayor o igual a 3 o menor igual a 12, se debe de mostrar True o false.

---
+++ Ejercicio6.py
@@ -0,0 +1,11 @@
+
+print ("¿La siguiente cadena tiene entre 3 y 12 carácteres?")
+
+frase = input()
+size = len (frase)
+
+
+if size>=3 and size<=12: 
+    print ("True")
+else: 
+    print ("False")
