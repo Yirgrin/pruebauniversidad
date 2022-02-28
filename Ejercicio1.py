0e2a71ce6ac8752b77ac8dcf902d63e0238fef0b
Yirgrin <ybermuddez@gmail.com>
2022-02-28 13:48:32 -0600

    Ejercicio 1. Python
    Asiganción #2
    Indicación: solicite 2 números por el teclado y debe de mostrar en pantalla la suma, resta, multiplicación y división de ellos.

---
+++ Ejercicio1.py
@@ -0,0 +1,19 @@
+
+from __future__ import division
+
+
+print ("Ingrese dos números")
+
+num1 = int (input ())
+num2 = int (input ())
+
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+
+print ("El resultado de la suma de estos dos números es de: " , suma)
+print ("El resultado de la resta de estos dos números es de: " , resta)
+print ("El resultado de la multiplicación de estos dos números es de: " , multiplicacion)
+print ("El resultado de la division de estos dos números es de: " , division)
+
